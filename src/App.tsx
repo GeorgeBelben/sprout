61eb66cdf200e204bd9eba277098933a70fa4821
George Belben <george.belben@perspicuity.co.uk>
2024-08-30 12:15:12 +0100

    Configured tailwind and added splash screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import { Logo } from "./components/logo";
 
 export function App(): ReactElement {
 	return (
-		<div>
-			<Logo />
+		<div className="bg-neutral-900 h-[100dvh] flex items-center justify-center">
+			<Logo color="white" size="lg" className="opacity-20" />
 		</div>
 	);
 }
--- src/types/helpers.ts
+++ src/types/helpers.ts
@@ -1,3 +1,5 @@
 export type Brand<K> = K & { __brand: "brand" };
 
 export type ErrorLike = { message: string };
+
+export type PropsWithClassName<T> = { className?: string } & T;
